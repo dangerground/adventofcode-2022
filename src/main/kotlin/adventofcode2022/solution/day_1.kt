9a40bfdd82ced6bedca8d32bd3ef60d225da2bf4
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2022-12-17 10:36:58 +0100

    Day 1, Part 2 solution

--- src/main/kotlin/adventofcode2022/solution/day_1.kt
+++ src/main/kotlin/adventofcode2022/solution/day_1.kt
@@ -10,10 +10,17 @@ fun main() {
     println("Day $DAY_NUM")
 
     println("Part 1 solution: ${solution1(inputText)}")
+    println("Part 2 solution: ${solution2(inputText)}")
 }
 
 fun solution1(inputText: String): Long? {
     val input = batchedList(inputText.lines().map { it.toLongOrNull() })
 
     return input.maxOfOrNull { batch -> batch.sumOf { it ?: 0 } }
+}
+
+fun solution2(inputText: String): Long? {
+    val input = batchedList(inputText.lines().map { it.toLongOrNull() })
+
+    return input.map { batch -> batch.sumOf { it ?: 0 } }.sortedDescending().take(3).sum()
 }
